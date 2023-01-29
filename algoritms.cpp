c16530a09921e6b9d383c7bb161a6a609e09265b
Vija Vagale <vija.vagale@gmail.com>
2023-01-29 20:28:32 +0200

    Create algoritms.cpp

---
+++ algoritms.cpp
@@ -0,0 +1,12 @@
+//Uzrakstīt programmu C++, kas sakārto skaitļus augošā secībās. Izmantot skaitļu masīvu.
+//Lineārais algoritms.
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+
+system("pause>nul");
+return 0;
+}
